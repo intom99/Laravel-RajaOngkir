61216afd734198e8e8596ce14e7aa9887bb06f86
intom <iin.arahmah99@gmail.com>
2021-07-13 07:58:39 +0700

    make CheckOngkirController

---
+++ app/Http/Controllers/CheckOngkirController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\City;
+use App\Province;
+use Illuminate\Http\Request;
+use Kavist\RajaOngkir\Facades\RajaOngkir;
+
+class CheckOngkirController extends Controller
+{
+    public function index()
+    {
+        $provinces = Province::pluck('name', 'province_id');
+        return view('ongkir', compact('provinces'));
+    }
+
+    public function getCities($id)
+    {
+        $cities = City::where('province_id', $id)->pluck('name', 'city_id');
+        return response()->json($city);
+    }
+
+    public function check_ongkir(Request $request)
+    {
+        $costs = RajaOngkir::ongkir([
+            'origin' => $request->city_origin,
+            'destination' => $request->city_destination,
+            'weight' => $request->weight,
+            'courier' => $request->courier
+        ])->get();
+
+        return response()->json($costs);
+    }
+}
