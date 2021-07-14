b2a68e92ed4ec15ab7910c0a4167f88fae80646a
intom <iin.arahmah99@gmail.com>
2021-07-15 00:41:03 +0700

    update file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'CheckOngkirController@index');
+Route::post('/', 'CheckOngkirController@check_ongkir');
+Route::get('/cities/{province_id}', 'CheckOngkirController@getCities');
